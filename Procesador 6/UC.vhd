c26dd27c4cb759324ca6847bcc1af63090dcfad0
UTP <utp@CRIE.LOCAL>
2016-11-04 18:07:37 -0500

    UC pro4 + Branch

--- Procesador 6/UC.vhd
+++ Procesador 6/UC.vhd
@@ -6,11 +6,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity UC is
     Port ( op : in  STD_LOGIC_VECTOR (1 downto 0);
            op3 : in  STD_LOGIC_VECTOR (5 downto 0);
-           ALUOP : out  STD_LOGIC_VECTOR (5 downto 0);
+			  op2 : in  STD_LOGIC_VECTOR (2 downto 0);----------
+           icc : in  STD_LOGIC_VECTOR (3 downto 0);
 			  cond: in  STD_LOGIC_VECTOR (3 downto 0);
+			  
+			  ALUOP : out  STD_LOGIC_VECTOR (5 downto 0);
 			  rfdest : out  STD_LOGIC;
 			  rfsource : out  STD_LOGIC_VECTOR (1 downto 0);
 			  wrEnMem : out  STD_LOGIC;
+			  pcSource : out STD_LOGIC_VECTOR (1 downto 0);
            wrEnRF : out  STD_LOGIC);
 end UC;
 
@@ -20,6 +24,279 @@ begin
 
 	process(op,op3)
 	begin
+	
+		if(op = "00") then -- Branches Nop & Sethi
+			--Branch
+			if(op2 = "010")then 
+				case cond is
+					when "1000" => -- B Always
+						pcSource <= "10";
+						wrEnRF <= '0'; 
+						rfsource <= "00"; -- 
+						rfdest <= '0';
+						wrEnMem <= '0';
+						ALUOP <= "111111";
+					end if;
+						
+					-- NOTA: B Never ????
+					
+					when "1001" => -- B not equal
+						if (not(icc(2)) = '1') then -- not z
+							pcSource <= "10"; 
+							wrEnRF <= '0'; 
+							rfsource <= "00"; 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+							
+					when "0001" => -- B on equal
+						if (icc(2) = '1') then -- z
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+							
+					when "1001" => -- B Greather
+						if (not(icc(2) or (icc(3) xor icc(1))) = '1') then --
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+					
+					when "0010" => -- B less or equal
+						if (icc(2) or (icc(3) xor icc(1)) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+					
+					when "1011" => -- B greater or equal
+						if (not(icc(3) xor icc(1)) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+							
+					when "0011" => -- B less
+						if (icc(3) xor icc(1) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+					
+					when "1100" => -- B greater unsigned
+						if (not(icc(0) or icc(2)) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+				
+					when "0100" => -- B less or equal unsigned
+						if (icc(0) or icc(2) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+					
+					when "1101" => -- B on carry clear
+						if (not(0) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+				
+					when "0101" => -- B on carry set
+						if (icc(0) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+					
+					when "1110" => -- B on positive
+						if (not(icc(3)) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+					
+					when "0110" => -- B on negative
+						if (icc(3) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+					
+					when "1111" => -- B on overflow carry
+						if (not(icc(1)) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+					
+					when "0111" => -- B on overflow set
+						if (icc(1) = '1') then -- 
+							pcSource <= "10";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						else
+							pcSource <= "11";
+							wrEnRF <= '0'; 
+							rfsource <= "00"; -- 
+							rfdest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111";
+						end if;
+					
+					when others => -- Not Implemented
+							pcSource <= "00";
+							wrEnRF <= '0';
+							rfsource <= "00";
+							rfsest <= '0';
+							wrEnMem <= '0';
+							ALUOP <= "111111"; 
+					end case;
+				end if;
+			end if;
+		end if;
+					
+		------LOAD and STORE
+		
+		------Sethi
+		
+		------Call
+		
+		--- Modificar los otros
 		if(op = "10")then
 			case(op3) is
 				when "000001" => --AND
